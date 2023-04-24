a2a1b9b128af3b72249b1c402adb388c4769600c
d01c2 <d01c2@outlook.com>
2023-04-24 09:30:38 +0900

    seperated combined file

--- linked-list/custom.ml
+++ linked-list/custom.ml
@@ -12,23 +12,7 @@ module type STACK = sig
   val update : 'a stack * int * 'a -> 'a stack
 end
 
-module BuiltinListStack : STACK = struct
-  type 'a stack = 'a list
-
-  let empty = []
-  let isEmpty s = s = []
-  let cons (x, s) = x :: s
-  let head s = match s with [] -> raise EMPTY | hd :: _ -> hd
-  let tail s = match s with [] -> raise EMPTY | _ :: tl -> tl
-  let ( ++ ) = ( @ )
-
-  let rec update (s, i, x) =
-    match s with
-    | [] -> raise EMPTY
-    | hd :: tl -> if i = 0 then x :: tl else hd :: update (tl, i - 1, x)
-end
-
-module CustomStack : STACK = struct
+module Stack : STACK = struct
   type 'a stack = Nil | Cons of 'a * 'a stack
 
   let empty = Nil
