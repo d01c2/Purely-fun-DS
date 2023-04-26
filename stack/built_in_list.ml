7e80494aa43786b059be9a50d7b469f02e2d328a
d01c2 <d01c2@outlook.com>
2023-04-26 11:19:17 +0900

    fixed typos

--- stack/built_in_list.ml
+++ stack/built_in_list.ml
@@ -1,21 +1,22 @@
+(* Stack Implementation using Built-In List type *)
 exception EMPTY
 
 module type STACK = sig
   type 'a stack
 
-  val empty : 'a stack
-  val isEmpty : 'a stack -> bool
   val push : 'a * 'a stack -> 'a stack
-  val top : 'a stack -> 'a (* raises EMPTY* if stack is empty *)
-  val pop : 'a stack -> 'a stack (* raises EMPTY* if stack is empty *)
+  val top : 'a stack -> 'a
+  val pop : 'a stack -> 'a stack
+  val isEmpty : 'a stack -> bool
+  val popAll : 'a stack
 end
 
 module BuiltinListStack : STACK = struct
   type 'a stack = 'a list
 
-  let empty = []
-  let isEmpty s = s = []
   let push (x, s) = x :: s
   let top s = match s with [] -> raise EMPTY | hd :: _ -> hd
   let pop s = match s with [] -> raise EMPTY | _ :: tl -> tl
+  let isEmpty s = s = []
+  let popAll = []
 end
--- stack/custom.ml
+++ stack/custom.ml
@@ -1,21 +1,22 @@
+(* Stack Implementation using Custom data type *)
 exception EMPTY
 
 module type STACK = sig
   type 'a stack
 
-  val empty : 'a stack
-  val isEmpty : 'a stack -> bool
   val push : 'a * 'a stack -> 'a stack
-  val top : 'a stack -> 'a (* raises EMPTY* if stack is empty *)
-  val pop : 'a stack -> 'a stack (* raises EMPTY* if stack is empty *)
+  val top : 'a stack -> 'a
+  val pop : 'a stack -> 'a stack
+  val isEmpty : 'a stack -> bool
+  val popAll : 'a stack
 end
 
 module CustomStack : STACK = struct
   type 'a stack = Nil | Cons of 'a * 'a stack
 
-  let empty = Nil
-  let isEmpty s = match s with Nil -> true | _ -> false
   let push (x, s) = Cons (x, s)
   let top s = match s with Nil -> raise EMPTY | Cons (hd, _) -> hd
   let pop s = match s with Nil -> raise EMPTY | Cons (_, tl) -> tl
+  let isEmpty s = match s with Nil -> true | _ -> false
+  let popAll = Nil
 end
